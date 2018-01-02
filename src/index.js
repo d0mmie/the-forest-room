f3c02ecbe4432634152e06ea516695991784c7c0
d0mmie <st234@outlook.co.th>
2018-01-02 18:51:14 +0700

    Initialize firebase app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import firebase from 'firebase'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const config = {
+    apiKey: "AIzaSyDvf82RVmyi8V8GJIC8UVkCsNJ9Isrigak",
+    authDomain: "the-forest-room.firebaseapp.com",
+    databaseURL: "https://the-forest-room.firebaseio.com",
+    projectId: "the-forest-room",
+    storageBucket: "the-forest-room.appspot.com",
+    messagingSenderId: "1039388374848"
+}
+
+firebase.initializeApp(config)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
