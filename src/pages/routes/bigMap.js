356bb86c221748664ffd32f6fc86b620c20a760e
d0mmie <st234@outlook.co.th>
2018-01-03 20:05:06 +0700

    moved data to cloud

--- src/pages/routes/bigMap.js
+++ src/pages/routes/bigMap.js
@@ -1,14 +1,9 @@
+import _ from 'lodash'
 import firebase from 'firebase'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 import ImageMapper from '../../libs/ImageMapper'
-
-const MapInfo = [
-  {
-    path: '/map/4'
-  }
-]
 
 export default class BigMap extends React.Component {
   constructor (props) {
@@ -16,6 +11,7 @@ export default class BigMap extends React.Component {
     this.state = {
       mapUrl: '',
       map: {},
+      link: [],
       loading: true
     }
     this.MapNivigate = this.MapNivigate.bind(this)
@@ -28,6 +24,7 @@ export default class BigMap extends React.Component {
         this.setState({
           mapUrl: url,
           map: snap.val().map,
+          link: _.values(snap.val().link),
           loading: false
         })
       }
@@ -42,7 +39,7 @@ export default class BigMap extends React.Component {
   }
 
   MapNivigate (event, index) {
-    this.props.history.push(MapInfo[index].path)
+    this.props.history.push(this.state.link[index].path)
   }
 
   render () {
--- src/pages/routes/primaryMap.js
+++ src/pages/routes/primaryMap.js
@@ -2,14 +2,9 @@ import { Icon } from 'antd'
 import firebase from 'firebase'
 import PropTypes from 'prop-types'
 import React from 'react'
+import _ from 'lodash'
 
 import ImageMapper from '../../libs/ImageMapper'
-
-const MapInfo = [
-  {
-    path: '/map/4/11'
-  }
-]
 
 export default class PrimaryMap extends React.Component {
   constructor (props) {
@@ -17,6 +12,7 @@ export default class PrimaryMap extends React.Component {
     this.state = {
       mapUrl: '',
       map: {},
+      link: [],
       loading: true
     }
     this.MapNivigate = this.MapNivigate.bind(this)
@@ -29,6 +25,7 @@ export default class PrimaryMap extends React.Component {
         this.setState({
           mapUrl: url,
           map: snap.val().map,
+          link: _.values(snap.val().link),
           loading: false
         })
       }
@@ -43,7 +40,7 @@ export default class PrimaryMap extends React.Component {
   }
 
   MapNivigate (event, index) {
-    this.props.history.push(MapInfo[index].path)
+    this.props.history.push(this.state.link[index].path)
   }
 
   render () {
